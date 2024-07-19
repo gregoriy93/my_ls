a86a476ececc8a7251badfa8b7e62d8c7fb1b3b7
Gregoriy Bereznyakov <gregoriy93.gb@gmail.com>
2024-07-19 19:23:11 +0500

    Ведётся документирование в системе Doxygen

--- my_ls.c
+++ my_ls.c
@@ -1,3 +1,14 @@
+/**
+ * @file my_ls.c
+ * @author Gregoriy Bereznyakov (gregoriy93.gb@gmail.com)
+ * @brief Консольное приложение-аналог утилиты ls для тестового задания
+ * @date 2024-07-19
+ * 
+ * @copyright Square Enix (c) 2024
+ * 
+ */
+
+
 #include <stdio.h>
 
 int main(int argc, char* argv[])
