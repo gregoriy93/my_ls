d74f3d38b88438012def9b62d0a9dd7403f1261a
Gregoriy Bereznyakov <gregoriy93.gb@gmail.com>
2024-07-19 20:24:02 +0500

    Обработка входных параметров

--- my_ls.c
+++ my_ls.c
@@ -10,9 +10,59 @@
 
 
 #include <stdio.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <string.h>
+
 
 int main(int argc, char* argv[])
 {
-    printf("Hello\n");
+    bool list = false, reverse = false, non_bytes = false;
+
+    //Определение входных параметров
+    while(1)
+    {
+        int param = getopt(argc, argv, "lhr");
+
+        //Если был задан параметр, начинающийся на "-"
+        if(param != -1)
+        {
+            switch(param)
+            {
+                //выводит подробную таблицу файлов в директории
+                case 'l':
+                list = true;
+                break;
+
+                //изменяет формат вывода размеров файлов. Вместо размера в байтах
+                case 'h':
+                non_bytes = true;
+                break;
+
+                //изменяет порядок вывода файлов на обратный
+                case 'r':
+                reverse = true;
+                break;
+            }
+        }
+        else
+        {
+            //Это что-то другое (имя каталога)
+            optind++;
+        }
+
+        if(optind == argc) break;
+    }
+
+    //Для определения списка заданных каталогов
+    for (int i = 1; i < argc; i++)
+    {
+        if(argv[i][0] != '-')
+        {
+            printf("size: %d \n", strlen(argv[i]));
+        }
+    }
+    
+    
     return 0;
 }
